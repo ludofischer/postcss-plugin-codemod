8874eb4f6f5a444d2c812f9804793084a61bd756
Ludovico Fischer <livrerie@gmail.com>
2020-12-13 22:30:39 +0100

    Handle options and plugin return value

--- fixtures/example.js
+++ fixtures/example.js
@@ -1,3 +1,7 @@
 import postcss from 'postcss';
 
-export default postcss.plugin('example-plugin', function(opts) {})
+export default postcss.plugin('example-plugin', function (opts) {
+  if (opts.something) {
+  }
+  return function (css) {};
+});
--- transform.js
+++ transform.js
@@ -1,22 +1,57 @@
-module.exports = function(fileInfo, api) {
-  
+module.exports = function (fileInfo, api) {
   const j = api.jscodeshift;
-  const pluginCreatorAst = j.functionDeclaration(
-    j.identifier('pluginCreator'),
-    [j.identifier('opts')],
-    j.blockStatement([j.emptyStatement()])
-  )
+  const postCssAssignmentAST = j.assignmentExpression(
+    '=',
+    j.memberExpression(j.identifier('pluginCreator'), j.identifier('postcss')),
+    j.literal('true')
+  );
   const f = j(fileInfo.source);
-  f.find(j.CallExpression, {
+  const oldPluginDefinition = f.find(j.CallExpression, {
     callee: {
       type: 'MemberExpression',
       object: {
-        name: 'postcss'
-      }
-    }
-  }).replaceWith(pluginCreatorAst);
- 
-  
-  return f.toSource();
+        name: 'postcss',
+      },
+      property: {
+        name: 'plugin',
+      },
+    },
+  });
+  f.find(j.ImportDeclaration, {
+    type: 'ImportDeclaration',
+    source: {
+      type: 'Literal',
+      value: 'postcss',
+    },
+  }).remove();
 
-}
+  const oldReturnStatement = oldPluginDefinition
+    .nodes()[0]
+        .arguments[1].body.body.filter((node) => node.type === 'ReturnStatement');
+  const oldOptionHandling = oldPluginDefinition
+    .nodes()[0]
+    .arguments[1].body.body.filter((node) => node.type !== 'ReturnStatement');
+  const pluginName = oldPluginDefinition.nodes()[0].arguments[0];
+  const oldPluginBody = oldPluginDefinition.nodes()[0].arguments[1].body;
+  const oldArguments = oldPluginDefinition.nodes()[0].arguments[1].params;
+  const pluginCreatorAst = j.functionDeclaration(
+    j.identifier('pluginCreator'),
+    oldArguments,
+    j.blockStatement([
+      ...oldOptionHandling,
+      j.returnStatement(
+        j.objectExpression([
+          j.property('init', j.identifier('postcssPlugin'), pluginName),
+          j.property(
+            'init',
+            j.identifier('OnceExit'),
+            oldReturnStatement[0].argument
+          ),
+        ])
+      ),
+    ])
+  );
+
+  oldPluginDefinition.replaceWith(pluginCreatorAst);
+  return f.toSource();
+};
